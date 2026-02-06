411e247ec13f66ae4ffdf53fcfc48d79f4c59875
Stephy <stephaniemhay.deleon@lorma.edu>
2026-02-06 12:28:45 +0000

    Updated

--- activity3/Mission.java
+++ activity3/Mission.java
@@ -1,63 +1,52 @@
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Random;
 
-public class SecretAgent {
-    private final String agentId;
-    private String codename;
-    private int clearanceLevel;
-    private boolean onMission;
-    private LocalDateTime lastMissionCompletionTime;
+public class Mission {
+    // Fields
+    private String missionTarget;
+    private LocalDate missionDeadline;
+    private int difficulty;
 
-    // constructor
-    public SecretAgent(String agentId, String codename, int clearanceLevel) {
-        this.agentId = agentId;
-        this.codename = codename;
-        this.clearanceLevel = clearanceLevel;
-        this.onMission = false;
-        this.lastMissionCompletionTime = null;
+    // Constructor
+    public Mission(){
+        Random random = new Random();
+        String[] missionTargets = 
+            {"Retrieve stolen data", 
+            "Infiltrate enemy base", 
+            "Rescue hostage", 
+            "Sabotage communications", 
+            "Escort VIP"};
+
+        int randomMissionIndex = random.nextInt(missionTargets.length);
+        this.missionTarget = missionTargets[randomMissionIndex];
+
+        this.difficulty = random.nextInt(10) + 1;
+        int days = random.nextInt(24) + 7;
+        this.missionDeadline = LocalDate.now().plusDays(days);
     }
 
-    // Methods(getters)
-    public String getAgentId() {
-        return agentId;
-    }
-
-    public String getCodename() {
-        return codename;
-    }
-
-    public int getClearanceLevel() {
-        return clearanceLevel;
-    }
-
-    public boolean isOnMission() {
-        return onMission;
-    }
-
-    public LocalDateTime getLastMissionCompletionTime() {
-        return lastMissionCompletionTime;
-    }
-
-    // Methods(setters)
-    public void setCodename(String newCodename) {
-        this.codename = newCodename;
-    }
-
-    public void setClearanceLevel(int level) {
-        if (level >= 1 && level <= 5) {
-            this.clearanceLevel = level;
-        } else {
-            System.out.println("Invalid clearance level");
+    //Getters
+        public String getMissionTarget(){
+            return missionTarget;
         }
-    }
 
-    // Behavioral Methods
-    public void startMission() {
-        this.onMission = true;
-    }
+        public LocalDate getMissionDeadline(){
+            return missionDeadline;
+        }
 
-    public void completeMission() {
-        this.onMission = false;
-        this.lastMissionCompletionTime = LocalDateTime.now();
-    }
+        public int getDifficulty(){
+            return difficulty;
+        }
+
+        public void displayMissionBriefing(){
+            DateTimeFormatter date = DateTimeFormatter.ofPattern("MMMM dd, yyyy");
+            System.out.printf("""
+                    \n*** MISSION BRIEFING ***
+                    Target: %s
+                    Difficulty: %d
+                    Deadline: %s
+                    """, getMissionTarget(), getDifficulty(), getMissionDeadline().format(date));
+        }
 }
